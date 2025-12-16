4d1a518249a31c3a9c919e54b94441cb805557df
prakharv2001-creator <prakhar.v2001@gmail.com>
2025-12-16 22:25:54 +0400

    Add PinComposer import to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
+import PinComposer from '../components/PinComposer';
 
 interface Pin {
   id: string;
